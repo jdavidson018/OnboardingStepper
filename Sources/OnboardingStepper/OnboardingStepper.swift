73053b2425cfa6b3a998c751b07d0cb7119feb80
Jack Davidson <jdavidson018@gmail.com>
2024-02-04 18:57:53 -0700

    feat: Added First Component

--- Sources/OnboardingStepper/OnboardingStepper.swift
+++ Sources/OnboardingStepper/OnboardingStepper.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+struct OnboardingStepper: View {
+    var body: some View {
+        Text("Onboarding Stepper")
+    }
+}
+
+#Preview {
+    OnboardingStepper()
+}
