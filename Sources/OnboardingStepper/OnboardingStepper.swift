cbb4c764331071ac7d5d7dd1c42603f3bceb2006
Jack Davidson <jdavidson018@gmail.com>
2024-02-04 19:04:44 -0700

    fix: Made OnboardingStepper public
    This allows it to be imported by other packages

--- Sources/OnboardingStepper/OnboardingStepper.swift
+++ Sources/OnboardingStepper/OnboardingStepper.swift
@@ -2,8 +2,8 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 
-struct OnboardingStepper: View {
-    var body: some View {
+public struct OnboardingStepper: View {
+    public var body: some View {
         Text("Onboarding Stepper")
     }
 }
